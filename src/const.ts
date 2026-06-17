baf60eb7d32d10b6a5458dcce20eae6aebf5d14f
peterberg1-beep <peter.berg1@gmail.com>
2026-06-17 18:14:44 +0200

    Update const.ts

--- src/const.ts
+++ src/const.ts
@@ -1,4 +1,4 @@
-export const APP_LOGO = "/images/proforgehr-logo.png";                    // small circles
-export const APP_LOGO_HERO = "/images/proforgehr-hero-white.png";        // main hero
-export const APP_LOGO_HORIZONTAL = "/images/proforgehr-hero-white.png";
-export const APP_LOGO_DARK = "/images/proforgehr-hero-black.png";
+export const APP_LOGO = "/images/proforgehr-logo.png.jpg";
+export const APP_LOGO_HERO = "/images/proforgehr-hero-white.png.jpg";
+export const APP_LOGO_HORIZONTAL = "/images/proforgehr-hero-white.png.jpg";
+export const APP_LOGO_DARK = "/images/proforgehr-hero-black.png.jpg";
