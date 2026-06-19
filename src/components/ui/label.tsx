e5ce3c313350bcc0c20ede5ef79b9c52ed98c3af
peterberg1-beep <peter.berg1@gmail.com>
2026-06-19 10:54:25 +0200

    Update label.tsx

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { cn } from "@/lib/utils";
 
 const Label = React.forwardRef<
   HTMLLabelElement,
@@ -7,10 +6,7 @@ const Label = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <label
     ref={ref}
-    className={cn(
-      "flex items-center gap-2 text-sm font-medium leading-none select-none",
-      className
-    )}
+    className={`flex items-center gap-2 text-sm font-medium leading-none select-none ${className || ""}`}
     {...props}
   />
 ));
